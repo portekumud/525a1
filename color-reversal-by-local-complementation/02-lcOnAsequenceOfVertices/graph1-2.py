e742609fc621366373f7edc8850409f34d33be06
rajdeep <212011005@iitdh.ac.in>
2025-06-06 01:15:35 +0530

    second-code-done.Stuck-with-makefile

--- color-reversal-by-local-complementation/02-lcOnAsequenceOfVertices/graph1-2.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-G = nx.Graph() 
-plt.subplot(211)
-G.add_edges_from([(0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (2, 0), (2, 1), (2, 3), (3, 0), (3, 1), (3, 2), (3, 4), (4, 0), (4, 3)])
-nx.set_node_attributes(G, {0: 'green', 1: 'green', 2: 'yellow', 3: 'green', 4: 'yellow'}, name='color')
-nx.draw(G, with_labels=True, node_color=list(nx.get_node_attributes(G, 'color').values()))
-plt.savefig('graph1-2.pdf')
---
+++ color-reversal-by-local-complementation/02-lcOnAsequenceOfVertices/wao.sh
@@ -0,0 +1,4 @@
+python3 graph0-inputGraph.py
+python3 graph1-0.py
+python3 graph2-01.py
+python3 graph3-010.py
