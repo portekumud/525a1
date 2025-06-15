71770664d6a844fa6c55f213f1195e81ebad2eb8
rajdeep <212011005@iitdh.ac.in>
2025-06-15 20:11:59 +0530

    code2-completed

--- color-reversal-by-local-complementation/02-lcOnAsequenceOfVertices/graph11-01010312312.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-G = nx.Graph() 
-plt.subplot(211)
-G.add_edges_from([(0, 1), (0, 4), (1, 0), (1, 3), (1, 4), (2, 3), (2, 4), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2)])
-nx.set_node_attributes(G, {0: 'yellow', 1: 'green', 2: 'yellow', 3: 'green', 4: 'yellow'}, name='color')
-nx.draw(G, with_labels=True, node_color=list(nx.get_node_attributes(G, 'color').values()))
-plt.savefig('graph11-01010312312.png')
